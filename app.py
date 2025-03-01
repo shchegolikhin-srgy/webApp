08baa7a746b04626864045b923dd9e4fe1245337
shchegolikhin-srgy <shchegolikhin.sergey@gmail.com>
2025-03-02 00:56:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import psycopg2
 connection =psycopg2.connect(
     dbname = "projectdb",
     user = "postgres",
-    password="32081ABc",
+    password="",
     host="localhost",
     port = "5432"
 )
